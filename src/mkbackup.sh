90e0d0b46f062335affb92b7beed0a4483d8092c
jena0t <joaomouryf@hotmail.com>
2025-02-25 14:23:37 -0300

    refactor: script is now posix compliant

--- src/mkbackup.sh
+++ src/mkbackup.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 #==========================================================| HEADER
 #
@@ -14,6 +14,7 @@
 #		create a backup of them
 #
 #==========================================================| CHANGELOG
+#
 #	- 1.0:
 #		Supports lots of files and directories,
 #		creating a backup in the $HOME directory
@@ -35,8 +36,11 @@
 #		Corrected infinite loop when used
 #		other options
 #
-# - 1.3:
-#   Standardized output directory.
+#    - 1.3:
+#       Standardized output directory.
+#
+#   - 1.4:
+#       POSIX compliance.
 #
 #==========================================================#
 
@@ -46,17 +50,17 @@
 
 #==========================================================| GLOBAL VARIABLES
 
-VERSION='1.3'
+VERSION='1.4'
 OUT_DIR="$HOME/.backup_files" 	# the backup will be sent to the home of the user
 NAME='backup.tar.gz'
 
 #==========================================================| TESTS
 
-[ ! -d $OUT_DIR ] && mkdir -p "$OUT_DIR"
+[ ! -d "$OUT_DIR" ] && mkdir -p "$OUT_DIR"
 
 #==========================================================| FUNCTIONS
 
-function help() {
+help() {
 	cat <<'EOF'
 Usage: ./mkbackup [OPTIONS] [FILES]
 
@@ -73,24 +77,24 @@ Usage: ./mkbackup [OPTIONS] [FILES]
 EOF
 }
 
-function make_backup() {
+make_backup() {
 
 	# Verify if all the arguments points to valid files, otherwise exits with an error code
-	[ "$#" -eq "0" ] && { echo 'You must pass a command or files to backup' ; exit 1 ; }
+	[ "$#" -eq "0" ] && { printf 'You must pass a command or files to backup\n' ; exit 1 ; }
 
 	for i in "$@"; do
 		if [ -e "$i" ]; then
 			continue
 
 		else
-			echo '\e[31;1mYou select a file that do not exist.\e[0m'
+			printf '\e[31;1mYou select a file that do not exist.\e[0m\n'
 			exit 1
 		fi
 	done
 
-	echo -e "\e[39;1mCollecting the files\e[0m"
+	printf "\e[39;1mCollecting the files\e[0m\n"
 	tar --no-same-permissions -cpzf "${OUT_DIR:-.}/${NAME}" "$@" 1>/dev/null 2>&1
-	echo -e "\e[36;1mProcess completed\e[0m\nYou can find your backup in:\n${OUT_DIR:-.}/${NAME}"
+	printf "\e[36;1mProcess completed\e[0m\nYou can find your backup in:\n%s/%s\n" "${OUT_DIR:-.}" "${NAME}"
 
 }
 
@@ -104,7 +108,7 @@ while [ -n "$1" ]; do
 		;;
 
 		-v|--version)
-			echo "Version $VERSION"
+			printf "Version %s" "$VERSION"
 			shift
 		;;
 
